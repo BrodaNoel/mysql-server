45b91b99a6e4bd634197c6f62966476facedd71b
Broda Noel <brodanoel@gmail.com>
2016-01-29 18:45:37 -0300

    Add unzip function and replace __dirname in init funcion

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-var fs = require('fs');
+var fs = require('fs'),
+	zip = require('machinepack-zip');
 
 module.exports = {
 	humans: 'Only Broda Noel :(',
 	
 	init: function(){
-		// New my.ini
-		fs.createReadStream('./lib/xampp/mysql/bin/my.ini.backup').pipe(fs.createWriteStream('./lib/xampp/mysql/bin/my.ini'));
+		// Unzip xampp
+		zip.unzip({
+			source: './lib/xampp.zip',
+			destination: './lib'
+		}).exec({
+			error: function(data) {
+				console.log(data);
+			},
+			success: function() {
+				// New my.ini
+				fs.createReadStream('./lib/xampp/mysql/bin/my.ini.backup').pipe(fs.createWriteStream('./lib/xampp/mysql/bin/my.ini'));
 
-		// Replace all #FULLDIR# for ".";
-		fs.readFile('./lib/xampp/mysql/bin/my.ini', 'utf8', function (err, data) {
-			if (err) return console.log(err);
+				// Replace all #FULLDIR# for ".";
+				fs.readFile('./lib/xampp/mysql/bin/my.ini', 'utf8', function (err, data) {
+					if (err) return console.log(err);
 
-			var result = data.replace(/#FULLDIR#/g, __dirname);
+					var result = data.replace(/#FULLDIR#/g, __dirname);
 
-			fs.writeFile('./lib/xampp/mysql/bin/my.ini', result, 'utf8', function (err) {
-				if (err) return console.log(err);
-			});
+					fs.writeFile('./lib/xampp/mysql/bin/my.ini', result, 'utf8', function (err) {
+						if (err) return console.log(err);
+					});
+				});
+			}
 		});
 	}
 };

d13bce48092e685e0438197d577e253d2afeb937
Broda Noel <brodanoel@gmail.com>
2016-01-29 18:15:50 -0300

    Add initializing function

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+var fs = require('fs');
+
 module.exports = {
 	humans: 'Only Broda Noel :(',
 	
-	sayHello: function(){
-		return 'Hello!!! I\'m Noel, and you?'
+	init: function(){
+		// New my.ini
+		fs.createReadStream('./lib/xampp/mysql/bin/my.ini.backup').pipe(fs.createWriteStream('./lib/xampp/mysql/bin/my.ini'));
+
+		// Replace all #FULLDIR# for ".";
+		fs.readFile('./lib/xampp/mysql/bin/my.ini', 'utf8', function (err, data) {
+			if (err) return console.log(err);
+
+			var result = data.replace(/#FULLDIR#/g, __dirname);
+
+			fs.writeFile('./lib/xampp/mysql/bin/my.ini', result, 'utf8', function (err) {
+				if (err) return console.log(err);
+			});
+		});
 	}
 };
